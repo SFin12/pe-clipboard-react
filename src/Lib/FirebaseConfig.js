35935fc1edd351b3cc5b9d2a96c7c0e9dd8d9858
Sean Finegan <sean.j.finegan@gmail.com>
2021-12-08 21:37:32 -0800

    deleted old firebase code

--- src/Lib/FirebaseConfig.js
+++ src/Lib/FirebaseConfig.js
@@ -2,21 +2,6 @@
 import { initializeApp } from "@firebase/app";
 import { getDatabase } from "@firebase/database";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDvp9oidWEwc30gLlAabcc7pjOtq10wEU8",
-//     authDomain: "pe-clipboard-3391b.firebaseapp.com",
-//     projectId: "pe-clipboard-3391b",
-//     storageBucket: "pe-clipboard-3391b.appspot.com",
-//     messagingSenderId: "799915580717",
-//     appId: "1:799915580717:web:7e7048202d010d1fcfc7a4",
-//     measurementId: "G-EM25E400GB",
-//     databaseURL: "https://pe-clipboard-3391b-default-rtdb.firebaseio.com/",
-// };
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
