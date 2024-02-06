e54eff9e4d44561b1e6ffd9ff7289d05f7828bc4
Sean Finegan <sean.j.finegan@gmail.com>
2024-02-06 09:38:35 -0800

    DL shows for 600px

--- src/Pages/ClassesPage/StudentsPage/ClassDetailsPage/ClassDetailsPage2.js
+++ src/Pages/ClassesPage/StudentsPage/ClassDetailsPage/ClassDetailsPage2.js
@@ -138,7 +138,7 @@ function ClassDetailsPage(props) {
           <option value="weight">Weight</option>
           <option value="notes">Notes</option>
         </select>
-        {screenWidth > 700 && (
+        {screenWidth > 600 && (
           <CSVLink data={organizeStundenDetails(studentDetailInputs)} filename={`cc-scores-${currentClass}-${new Date().toLocaleDateString()}.csv`} className="btn btn-secondary btn-sm">
             Download
           </CSVLink>
